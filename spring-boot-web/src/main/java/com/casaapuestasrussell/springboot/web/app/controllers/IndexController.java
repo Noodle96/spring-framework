64e2baa0e008b846a933a183cb5bc9e7d0616861
Noodle96 <jorgealfredomarino2@gmail.com>
2021-09-09 16:57:17 -0500

    Mapear el controlador dandole el nombre de la ruta inicial

--- spring-boot-web/src/main/java/com/casaapuestasrussell/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/casaapuestasrussell/springboot/web/app/controllers/IndexController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	//http://localhost:8080/index
 	//DEFAULT => GET
-	@RequestMapping(value = {"/index","/","home"},method=RequestMethod.GET)
+	@RequestMapping(value = {"/index","/","","home"},method=RequestMethod.GET)
 	//@GetMapping(value ="..")
 	public String index(Model model) {//(ModelMap model) or (Map<String,Object> map) or (ModelAndView mv)
 		//mv.addObject("tiutlo","hola ...");
