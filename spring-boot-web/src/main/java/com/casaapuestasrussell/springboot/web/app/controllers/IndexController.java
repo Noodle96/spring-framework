45579785c402eb089ddd538e2c44e72c3543b660
Noodle96 <jorgealfredomarino2@gmail.com>
2021-09-09 16:47:40 -0500

    Pasando información del modelo a la vista

--- spring-boot-web/src/main/java/com/casaapuestasrussell/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/casaapuestasrussell/springboot/web/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.casaapuestasrussell.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +11,12 @@ public class IndexController {
 	//DEFAULT => GET
 	@RequestMapping(value = {"/index","/","home"},method=RequestMethod.GET)
 	//@GetMapping(value ="..")
-	public String index() {
-		
+	public String index(Model model) {//(ModelMap model) or (Map<String,Object> map) or (ModelAndView mv)
+		//mv.addObject("tiutlo","hola ...");
+		//mv.setViewName("index");
+		//return mv;
+		//map.put("titulo","hola ...");
+		model.addAttribute("titulo", "Titulo_Spring boot");
 		//return name vista => index.html
 		return "index";
 	}
