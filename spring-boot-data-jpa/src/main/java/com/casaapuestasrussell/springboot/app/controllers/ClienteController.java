74d0ac91b3d967b0866baa67ba5a7c7eae1baf31
Noodle96 <jorgealfredomarino2@gmail.com>
2022-01-18 00:08:12 -0500

    La consola H2 database

--- spring-boot-data-jpa/src/main/java/com/casaapuestasrussell/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/casaapuestasrussell/springboot/app/controllers/ClienteController.java
@@ -1,4 +1,4 @@
- package com.casaapuestasrussell.springboot.app.controllers;
+package com.casaapuestasrussell.springboot.app.controllers;
 
 import java.util.Map;
 
@@ -12,30 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.casaapuestasrussell.springboot.app.models.dao.IClienteDao;
 import com.casaapuestasrussell.springboot.app.models.entity.Cliente;
 
-
 @Controller
 public class ClienteController {
-	
+
 	@Autowired
 	@Qualifier("clienteDaoJPA")
 	private IClienteDao clienteDao;
-	
-	@RequestMapping(value ="listar",method=RequestMethod.GET)
-	public String listar(Model model) {		
+
+	@RequestMapping(value = "listar", method = RequestMethod.GET)
+	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de Clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
-	
-	@RequestMapping(value="/form")
+
+	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
 		model.put("titulo", "Formulario del Cliente");
 		model.put("cliente", cliente);
 		return "form";
 	}
-	
-	@RequestMapping(value="/form", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(Cliente cliente) {
 		clienteDao.save(cliente);
 		return "redirect:listar";
